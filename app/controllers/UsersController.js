f1ddbf39b8343ddb45c7057a266daaf5e7e85f7c
Md. Fawjul Azim <mdfawjulazim617@gmail.com>
2024-11-17 01:05:30 +0600

    ProfileDetails

--- app/controllers/UsersController.js
+++ app/controllers/UsersController.js
@@ -2,7 +2,7 @@ import UserModel from "../models/UserModel.js";
 import { EncodeToken } from './../utility/tokenUtility.js';
 
 
-
+//User Registration
 export const Registration = async (req,res)=>{
     try{
         let reqBody = req.body;
@@ -13,25 +13,42 @@ export const Registration = async (req,res)=>{
     }
 }
 
-export const Login = async (req,res)=>{
-    try{
-        let reqBody = req.body;
-        let data = await UserModel.findOne(reqBody);
+//user login
+export const Login=async(req,res)=>{
+
+    try {
+        let reqBody=req.body;
+        let data=await UserModel.findOne(reqBody)
         if(data===null){
-            return res.status(404).json({status:"fail","Message":"User not found"})
+            return res.json({status:"fail","Message":"User not found"})
         }
-        else{
-            //Login Successfully Token Encoded
-            let token = EncodeToken(data['email',data['_id']]);
-            return res.json({status:"success","Message":"User Login Successfully","token":token})
+        else {
+            // Login Success Token Encode
+            let token=EncodeToken(data['email'],data['_id'])
+            return res.json({status:"success",Token:token,"Message":"User Login successfully"})
         }
-    }catch(err){
-        return res.status(400).json({status:"fail","Message":err.toString()})
+
+    }
+    catch(err){
+        return res.json({status:"fail","Message":err.toString()})
+    }
+
+}
+
+//User Profile Details
+export const ProfileDetails=async(req,res)=>{
+    try {
+        let user_id=req.headers['user_id'];
+        let data=await UserModel.findOne({"_id":user_id})
+        return res.json({status:"success","Message":"User ProfileDetails successfully",data:data})
+    }
+    catch(err){
+        return res.json({status:"fail","Message":err.toString()})
     }
 }
-export const ProfileDetails = async (req,res)=>{
-    return res.json({status:"success","Message":"User ProfileDetails Successfully"})
-}
+
+
+
 export const ProfileUpdate = async (req,res)=>{
     return res.json({status:"success","Message":"User ProfileUpdate Successfully"})
 }
--- routes/api.js
+++ routes/api.js
@@ -3,6 +3,7 @@ const router = express.Router();
 
 import * as TaskController from "../app/controllers/TaskController.js";
 import * as UsersController from "../app/controllers/UsersController.js";
+import AuthMiddleware from "../app/middlewares/AuthMiddleware.js";
 
 
 
@@ -10,19 +11,19 @@ import * as UsersController from "../app/controllers/UsersController.js";
 //users 
 router.post('/Registration',UsersController.Registration);
 router.post('/Login',UsersController.Login);
-router.get('/ProfileDetails',UsersController.ProfileDetails);
-router.post('/ProfileUpdate',UsersController.ProfileUpdate);
+router.get('/ProfileDetails',AuthMiddleware,UsersController.ProfileDetails);
+router.post('/ProfileUpdate',AuthMiddleware,UsersController.ProfileUpdate);
 router.get('/EmailVerify',UsersController.EmailVerify);
 router.post('/CodeVerify',UsersController.CodeVerify);
 router.post('/ResetPassword',UsersController.ResetPassword);
 
 
 //Task 
-router.post('/CreateTask',TaskController.CreateTask);
-router.get('/UpdateTaskStatus',TaskController.UpdateTaskStatus);
-router.get('/TaskListByStatus',TaskController.TaskListByStatus);
-router.get('/DeleteTask',TaskController.DeleteTask);
-router.get('/CountTask',TaskController.CountTask);
+router.post('/CreateTask',AuthMiddleware,TaskController.CreateTask);
+router.get('/UpdateTaskStatus',AuthMiddleware,TaskController.UpdateTaskStatus);
+router.get('/TaskListByStatus',AuthMiddleware,TaskController.TaskListByStatus);
+router.get('/DeleteTask',AuthMiddleware,TaskController.DeleteTask);
+router.get('/CountTask',AuthMiddleware,TaskController.CountTask);
 
 
 
