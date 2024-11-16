a1713ba62667af79ecf0bfe5186fdb20000539bc
Md. Fawjul Azim <mdfawjulazim617@gmail.com>
2024-11-16 17:12:54 +0600

    Project finnal file stracture done..

---
+++ app/config/config.js
@@ -0,0 +1,26 @@
+
+
+// export const MONGODB_CONNECTION="mongodb+srv://mrrabbil:mernx123@cluster0.rtpbcy6.mongodb.net/app_todo?retryWrites=true&w=majority";
+export const MONGODB_CONNECTION="mongodb+srv://mdfawjulazim:mdfawjulazim123@cluster.szrub.mongodb.net/";
+
+
+export const JWT_SECRET = "5EC7CEFA1BE7C9354A639369A2AA8JS";
+export const JWT_EXPIRATION_TIME = 60*60*24*30;
+
+export const EMAIL_HOST = "sandbox.smtp.mailtrap.io";
+export const EMAIL_PORT = "2525";
+export const EMAIL_USER = "36c1cf0b7f496f";
+export const EMAIL_PASSWORD = "91480c498acc52";
+export const MAIL_ENCRYPTION=""; //as needed for the mailtrap
+
+export const Max_JSON_SIZE = "50mb";
+export const URL_ENCODER = true;
+
+
+export const REQUEST_LIMIT_TIME = 15*60*1000;//15 min
+export const REQUEST_LIMIT_NUMBER =3000;//per 15 Min 3000 Requests Allowed
+
+
+export const WEB_CACHE=false;
+export const PORT=5050;
+
