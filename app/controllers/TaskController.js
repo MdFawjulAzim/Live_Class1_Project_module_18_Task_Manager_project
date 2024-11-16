bdca8fe6a9840a4e7e2cacd27e9b48ed264a23b0
Md. Fawjul Azim <mdfawjulazim617@gmail.com>
2024-11-16 22:46:50 +0600

    Postman initial Config

---
+++ app/controllers/TaskController.js
@@ -0,0 +1,18 @@
+
+
+export const CreateTask = async (req,res)=>{
+    return res.json({status:"success","Message":"User CreateTask Successfully"})
+}
+
+export const UpdateTaskStatus = async (req,res)=>{
+    return res.json({status:"success","Message":"User UpdateTaskStatus Successfully"})
+}
+export const TaskListByStatus = async (req,res)=>{
+    return res.json({status:"success","Message":"User TaskListByStatus Successfully"})
+}
+export const DeleteTask = async (req,res)=>{
+    return res.json({status:"success","Message":"User DeleteTask Successfully"})
+}
+export const CountTask = async (req,res)=>{
+    return res.json({status:"success","Message":"User CountTask Successfully"})
+}
---
+++ app/controllers/UsersController.js
@@ -0,0 +1,26 @@
+
+
+
+export const Registration = async (req,res)=>{
+    return res.json({status:"success","Message":"User Registration Successfully"})
+}
+
+export const Login = async (req,res)=>{
+    return res.json({status:"success","Message":"User Login Successfully"})
+}
+export const ProfileDetails = async (req,res)=>{
+    return res.json({status:"success","Message":"User ProfileDetails Successfully"})
+}
+export const ProfileUpdate = async (req,res)=>{
+    return res.json({status:"success","Message":"User ProfileUpdate Successfully"})
+}
+export const EmailVerify = async (req,res)=>{
+    return res.json({status:"success","Message":"User EmailVerify Successfully"})
+}
+export const CodeVerify = async (req,res)=>{
+    return res.json({status:"success","Message":"User CodeVerify Successfully"})
+}
+
+export const ResetPassword = async (req,res)=>{
+    return res.json({status:"success","Message":"User ResetPassword Successfully"})
+}
