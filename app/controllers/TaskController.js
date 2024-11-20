e363841f319d717bae9bd94a3d85d0cd281d911f
Md. Fawjul Azim <mdfawjulazim617@gmail.com>
2024-11-21 01:58:37 +0600

    Create Task

--- app/controllers/TaskController.js
+++ app/controllers/TaskController.js
@@ -1,7 +1,17 @@
-
+import TaskModel from "../models/TaskModel.js"
 
 export const CreateTask = async (req,res)=>{
-    return res.json({status:"success","Message":"User CreateTask Successfully"})
+    try{
+        let user_id = req.headers['user_id'];
+        let reqBody = req.body;
+        reqBody.user_id = user_id;
+
+        await TaskModel.create(reqBody);
+        return res.json({status:"success","Message":"User CreateTask Successfully"});
+
+    }catch(err){
+        return res.json({status:"fail","Message":err.toString()})
+    }
 }
 
 export const UpdateTaskStatus = async (req,res)=>{
